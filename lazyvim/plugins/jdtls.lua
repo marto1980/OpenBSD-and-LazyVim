64ad239bab7126d5bb16f6a2b8b78b07098b6e22
Martin Ivanov <marto1980@gmail.com>
2026-02-26 18:01:50 +0200

    Improve jdtls Configuraion

--- lazyvim/plugins/jdtls.lua
+++ lazyvim/plugins/jdtls.lua
@@ -1,11 +1,21 @@
 return {
   {
     "mfussenegger/nvim-jdtls",
+    ft = "java",
     opts = function(_, opts)
-      local jdtls_base = "/home/marto/build/jdt-language-server"
+      -- Base paths
+      local home = os.getenv("HOME")
+      local jdtls_base = home .. "/build/jdt-language-server"
       local launcher_jar = vim.fn.glob(jdtls_base .. "/plugins/org.eclipse.equinox.launcher_*.jar")
       local config_dir = jdtls_base .. "/config_linux"
-      local workspace_dir = os.getenv("HOME") .. "/.cache/jdtls/workspace"
+      -- Root detection
+      local root_dir = vim.fs.root(0, { "gradlew", ".git", "mvnw", "pom.xml", "build.gradle" })
+      if not root_dir then
+        return
+      end
+      -- Project-specific workspace
+      local project_name = vim.fn.fnamemodify(root_dir, ":p:h:t")
+      local workspace_dir = home .. "/.cache/jdtls/workspace/" .. project_name
 
       -- We manually define the full cmd here to satisfy LazyVim's expectations
       opts.jdtls = {
@@ -28,6 +38,31 @@ return {
           config_dir,
           "-data",
           workspace_dir,
+        },
+        root_dir = root_dir,
+        settings = {
+          java = {
+            configuration = {
+              runtimes = {
+                {
+                  name = "JavaSE-17",
+                  path = "/usr/local/jdk-17",
+                },
+                {
+                  name = "JavaSE-21",
+                  path = "/usr/local/jdk-21",
+                },
+                {
+                  name = "JavaSE-25",
+                  path = "/usr/local/jdk-25",
+                },
+              },
+            },
+          },
+        },
+        -- for debug support at a later stage
+        init_options = {
+          bundles = {},
         },
       }
 
